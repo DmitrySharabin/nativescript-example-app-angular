861de9b3d9b2bbbbb8b8f75a03588f7bffaf56f0
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-09-24 15:52:07 +0200

    feat: Add basic layout

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
 
+import { FlickService } from "~/app/core/services";
+
 @Component({
 	moduleId: module.id,
 	selector: "ns-home",
 	templateUrl: "home.component.html",
 })
-export class HomeComponent {}
+export class HomeComponent {
+	flicks = this.flickService.getFlicks();
+
+	constructor(private flickService: FlickService) {}
+}
